693779b4d1199942b003e9c7601c4d2b62ca4136
fabioalxk <fabioalxk@users.noreply.github.com>
2018-06-04 17:21:33 -0300

    scroll record
    scrool record

--- client/js/global.js
+++ client/js/global.js
@@ -1,2 +1,13 @@
 var recordStatus = new Array();
 recordStatus["ONBDD"] = "Onboarded";
+
+$(function(){
+    $(".wrapper1").scroll(function(){
+        $(".wrapper2")
+            .scrollLeft($(".wrapper1").scrollLeft());
+    });
+    $(".wrapper2").scroll(function(){
+        $(".wrapper1")
+            .scrollLeft($(".wrapper2").scrollLeft());
+    });
+});
